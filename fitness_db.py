5132fbf251b8a2cd54424a90951d7c171746ee3b
Kush Aswani <kush17.aswani@gmail.com>
2023-08-17 17:27:09 +0300

    added code to create a user_health table to track weight and daily sleep hours,return weight on a specific day and documentation

--- fitness_db.py
+++ fitness_db.py
@@ -2,6 +2,12 @@ import sqlite3
 import database
 from datetime import datetime,timedelta
 
+"""
+Database script for user fitness and health, functionalities include:
+creation and deletion of fitness and user_health tables
+adding user fitness data (calories,carbs,protein,fats)
+retrieving averages for number of days for user fitness & health
+"""
 
 def create_table():
     # create a table if one does not exist already
@@ -9,14 +15,26 @@ def create_table():
     curr = conn.cursor()
 
     curr.execute("CREATE TABLE IF NOT EXISTS fitness (username TEXT,date DATE,calories INT,carbs INT,protein INT,fats INT)")
+    curr.execute("CREATE TABLE IF NOT EXISTS user_health (username TEXT,date DATE,weight REAL,sleep_hours REAL)")
     conn.commit()
     conn.close()
 
-def add_record(username,date,calories,carbs,protein,fats):
+def add_fitness(username,date,calories,carbs,protein,fats):
+    """
+    
+    """
     conn = sqlite3.connect("fitness.db")
     curr = conn.cursor()
 
     curr.execute("INSERT INTO fitness VALUES (?,?,?,?,?,?)",(username,date,calories,carbs,protein,fats))
+    conn.commit()
+    conn.close()
+
+def add_health(username,date,weight,sleep_hours):
+    conn = sqlite3.connect("fitness.db")
+    curr = conn.cursor()
+
+    curr.execute("INSERT INTO user_health VALUES (?,?,?,?)",(username,date,weight,sleep_hours))
     conn.commit()
     conn.close()
 
@@ -37,13 +55,7 @@ def return_total(username,date):
         total_protein += record[5]
         total_fat += record[6]
     return([total_cal,total_carb,total_protein,total_fat])
-    
-
-
-
-
-    
-    
+     
 def average_calories(username,days):
     current_date = datetime.now().date()
     end_date = current_date
@@ -65,6 +77,8 @@ def average_calories(username,days):
     return average_calories
 
 def average_carbs(username,days):
+    if days == 0:
+        return -2 
     current_date = datetime.now().date()
     end_date = current_date
     start_date = current_date - timedelta(days=days - 1)
@@ -75,7 +89,7 @@ def average_carbs(username,days):
     curr.execute("SELECT SUM(carbs) FROM fitness WHERE username=? AND date BETWEEN ? AND ?", (username,start_date,end_date))
     sum_carbs = curr.fetchone()[0]
 
-    curr.execute("SELECT COUNT(calories) FROM fitness WHERE username=? AND date BETWEEN ? AND ?",(username,start_date,end_date))
+    curr.execute("SELECT COUNT(carbs) FROM fitness WHERE username=? AND date BETWEEN ? AND ?",(username,start_date,end_date))
     records = curr.fetchone()[0]
     if records == 0:
         return -1
@@ -83,3 +97,71 @@ def average_carbs(username,days):
     conn.close()
     average_carbs = sum_carbs/records
     return average_carbs
+
+def average_protein(username,days):
+    current_date = datetime.now().date()
+    end_date = current_date
+    start_date = current_date - timedelta(days=days - 1)
+
+    conn = sqlite3.connect("fitness.db")
+    curr = conn.cursor()
+
+    curr.execute("SELECT SUM(protein) FROM fitness WHERE username=? AND date BETWEEN ? AND ?", (username,start_date,end_date))
+    sum_protein = curr.fetchone()[0]
+
+    curr.execute("SELECT COUNT(protein) FROM fitness WHERE username=? AND date BETWEEN ? AND ?",(username,start_date,end_date))
+    records = curr.fetchone()[0]
+    if records == 0:
+        return -1
+    conn.commit()
+    conn.close()
+    average_protein = sum_protein/records
+    return average_protein
+
+def average_fats(username,days):
+    current_date = datetime.now().date()
+    end_date = current_date
+    start_date = current_date - timedelta(days=days - 1)
+
+    conn = sqlite3.connect("fitness.db")
+    curr = conn.cursor()
+
+    curr.execute("SELECT SUM(protein) FROM fitness WHERE username=? AND date BETWEEN ? AND ?", (username,start_date,end_date))
+    sum_protein = curr.fetchone()[0]
+
+    curr.execute("SELECT COUNT(protein) FROM fitness WHERE username=? AND date BETWEEN ? AND ?",(username,start_date,end_date))
+    records = curr.fetchone()[0]
+    if records == 0:
+        return -1
+    conn.commit()
+    conn.close()
+    average_protein = sum_protein/records
+    return average_protein
+
+def all_fitness_averages(username,days):
+    return( [average_calories(username,days) , average_carbs(username,days) , average_protein(username,days), average_fats(username,days)] )
+
+def return_weight(username,date):
+    """
+    function return_weight
+    takes two parameters username (string) and date(date)
+    returns user weight on the given date or -1 if no record for user_health can be found on that date
+    """
+    conn = sqlite3.connect("fitness.db")
+    curr = conn.cursor()
+
+    curr.execute("SELECT weight FROM user_health WHERE username=? AND date=?",(username,date))
+    result = curr.fetchone()
+    if not result:
+        return -1
+    weight = result[0]
+    return weight
+
+def clear_table():
+    conn = sqlite3.connect("fitness.db")
+    curr = conn.cursor()
+
+    curr.execute("DELETE FROM fitness")
+    curr.execute("DELETE FROM user_health")
+    conn.commit()
+    conn.close()
